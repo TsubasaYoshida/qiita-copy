30f14555d77953c66413f95b2a8391966df896fc
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-01-26 23:20:00 +0900

    サインアップ機能を実装する #5

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :screen_name,
+            presence: true,
+            length: {maximum: 20},
+            uniqueness: true
+  validates :email,
+            presence: true,
+            length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: true
+  validates :password,
+            presence: true,
+            length: {minimum: 6}
+end
