0b404cb9c9295acbdc1dab0d0085c37b2f2a6f8c
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-02-02 12:27:23 +0900

    Tagsリソースについてルーティングを整備する #15

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :tags
   resources :drafts
   resources :users, only: [:new, :create, :destroy]
   get 'home/index'
   get ':screen_name', to: 'users#show'
   get ':screen_name/items/:id', to: 'items#show'
   delete ':screen_name/items/:id', to: 'items#destroy'
+  get 'tags/:name', to: 'tags#show'
   get '/deactivate', to: 'users#destroy'
   root to: 'home#index'
 end
