79817ce0e62768d78dc05eec4ebbd7d1e150ab11
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-02-02 13:16:43 +0900

    タグ入力フォームを実装する #15

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -5,6 +5,7 @@ class Draft < ApplicationRecord
   belongs_to :user
   has_one :item
 
+  attr_accessor :tags
   attr_accessor :type
 
   BEFORE_POST = %w(post limited_post save)
@@ -19,5 +20,9 @@ class Draft < ApplicationRecord
             length: {maximum: 100_000}
   validates :type,
             inclusion: {in: %w(post limited_post save)}
+  validates :tags,
+            presence: true,
+            # 最大50文字のタグが5つまで＝250文字、スペースで区切るためそのスペース分＝4文字
+            length: {maximum: 254}
 
 end
