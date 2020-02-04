792bc74ba3d047457b267162548bafaa3b5fe641
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-02-04 23:09:26 +0900

    itemが1つも紐づいていないタグの詳細ページにアクセスすると、404になるようにする #17

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,8 @@ class TagsController < ApplicationController
   skip_before_action :check_logged_in
 
   def show
-    # TODO item が1つも紐づいていないなら、404にしたい
+    # item が1つも紐づいていないなら、404にする
     @tag = Tag.find_by!(name: params[:name])
+    raise ActiveRecord::RecordNotFound if @tag.items.size.zero?
   end
 end
