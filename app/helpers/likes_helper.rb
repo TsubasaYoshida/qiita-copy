f37e8986c04f94ab1b0162f1c863927421ecbfca
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-02-24 00:53:20 +0900

    トップページのビューをリファクタリングする #39

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -7,7 +7,7 @@ module LikesHelper
 
   def pressed_like?(item)
     # 未ログイン状態でも呼ばれるため、lonely-operatorを使用する
-    Like.exists?(user_id: current_user&.id, item_id: item.id)
+    'item__thumbs-up-pressed' if Like.exists?(user_id: current_user&.id, item_id: item.id)
   end
 
   def get_contribution_ranking
