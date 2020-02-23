ab394af905b424a9658dccd4011c42472aecb863
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-02-24 00:36:22 +0900

    記事の並び順を修正する #39

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :check_logged_in
 
   def index
-    @items = Item.all
+    @items = Item.all.recent
   end
 end
--- app/helpers/drafts_helper.rb
+++ app/helpers/drafts_helper.rb
@@ -1,8 +1,9 @@
 module DraftsHelper
   def get_drafts
     # 未投稿のものと、投稿後に編集したもの
-    current_user.drafts.select do |draft|
+    drafts = current_user.drafts.select do |draft|
       !draft.item || draft.edit_after_posting
     end
+    drafts.sort_by! {|draft| draft[:updated_at]}.reverse!
   end
 end
